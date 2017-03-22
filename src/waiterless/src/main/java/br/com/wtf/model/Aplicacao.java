cd46d6608831e816e3f6267e3db17ba9d027f327
AlbertoJr <alberto.duartao@hotmail.com>
2017-03-21 21:42:39 -0300

    ItemController e AplicaçãoJava criados

---
+++ src/waiterless/src/main/java/br/com/wtf/model/Aplicacao.java
@@ -0,0 +1,15 @@
+package br.com.wtf.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import ola.Aplicacao;
+
+@SpringBootApplication
+public class Aplicacao {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Aplicacao.class, args);
+	}
+
+}
