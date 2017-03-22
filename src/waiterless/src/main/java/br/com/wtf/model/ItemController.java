e64b3c2255da7e8bf146e64794218b79b7b62295
Gabriel Costa <gabrielcosta80@gmail.com>
2017-03-22 20:23:56 -0300

    Incremento Html

--- src/waiterless/src/main/java/br/com/wtf/model/ItemController.java
+++ src/waiterless/src/main/java/br/com/wtf/model/ItemController.java
@@ -1,4 +1,22 @@
+package ola;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class ItemController {
+	
+    @GetMapping("/cadarpio")
+    public String ItemForm(Model model) {
+        model.addAttribute("item", new Item());
+        return "cardapio";
+    }
 
+    @PostMapping("/cardapio")
+    public String ItemSubmit(@ModelAttribute Item item) {
+        return "resultado";
+    }
 }
