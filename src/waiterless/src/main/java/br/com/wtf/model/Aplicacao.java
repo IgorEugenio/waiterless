0f426bf9a4d2ae044be1da31f95dc69f7d1e1998
Gabriel Costa <gabrielcosta80@gmail.com>
2017-03-22 21:16:43 -0300

    Correção de erros / Configuração Postgresql

--- src/waiterless/src/main/java/br/com/wtf/model/Aplicacao.java
+++ src/waiterless/src/main/java/br/com/wtf/model/Aplicacao.java
@@ -3,8 +3,6 @@ package br.com.wtf.model;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import ola.Aplicacao;
-
 @SpringBootApplication
 public class Aplicacao {
 	
--- src/waiterless/src/main/java/br/com/wtf/model/ItemController.java
+++ src/waiterless/src/main/java/br/com/wtf/model/ItemController.java
@@ -1,22 +1,20 @@
-package ola;
+package br.com.wtf.model;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ItemController {
 	
-    @GetMapping("/cadarpio")
-    public String ItemForm(Model model) {
+    @GetMapping("/cardapio")
+    public String itemForm(Model model) {
         model.addAttribute("item", new Item());
         return "cardapio";
     }
 
-    @PostMapping("/cardapio")
+ /*   @PostMapping("/cardapio")
     public String ItemSubmit(@ModelAttribute Item item) {
         return "resultado";
-    }
+    }*/
 }
